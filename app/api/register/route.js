40287a0ac0baf1bd021cba6cad371a9ddc86762c
Don Artkins <opiyodon9@gmail.com>
2023-09-17 17:42:05 +0300

    just created the api and registered a user

--- app/api/register/route.js
+++ app/api/register/route.js
@@ -1,12 +1,15 @@
+import { connectMongoDB } from "@/lib/mongodb";
 import { NextResponse } from "next/server";
+import bcrypt from "bcryptjs";
+import User from "@/models/user";
 
 export async function POST(req) {
   try {
     const { name, email, password } = await req.json();
-
-    console.log("Name:", name);
-    console.log("Email:", email);
-    console.log("Password:", password);
+    const hashedPassword = await bcrypt.hash(password, 10);
+    await connectMongoDB();
+    await User.create({ name, email, password: hashedPassword });
+    
 
     return NextResponse.json({ message: "User registered.." }, { status: 201 });
   } catch (error) {
