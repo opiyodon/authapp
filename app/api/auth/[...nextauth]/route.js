ecfcb8ef82be68e35d896437baebf2cc8fc2f3f4
Don Artkins <opiyodon9@gmail.com>
2023-09-18 12:34:58 +0300

    FINISHED PROJECT: handled redirects to pages

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -4,7 +4,7 @@ import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import bcrypt from "bcryptjs"; // Import bcrypt
 
-const authOptions = {
+export const authOptions = {
     providers: [
         CredentialsProvider({
             name: "credentials",
